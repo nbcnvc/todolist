c9c44bfe187c4fa0bf1e9d203da2b956039c81b5
nbcnvc <nbcnvc.dev@gmail.com>
2023-07-07 01:43:46 +0900

    Make App component fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 import TodoList from "./components/TodoList";
 
+const URL =
+  "https://todolist-a15fc-default-rtdb.asia-southeast1.firebasedatabase.app/todos.json";
+
 function App() {
-  return (
-    <TodoList />
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(URL);
+      const data = res.data;
+      return data
+    };
+
+    fetchData();
+  }, []);
+
+  return <TodoList />;
 }
 
 export default App;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,4 +12,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export default app;
-export const db = getFirestore(app);
+
